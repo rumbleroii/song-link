72bccb9ff08626f2a539cc90eea7be1f25854fd2
rumbleroii <mrithik2010@gmail.com>
2022-02-09 11:25:07 +0530

    Updates

---
+++ precache-manifest.76e99ee4fa7a07542bf292a7fa392b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937f4fcd0f00d6c5c6c5abfac70a184c",
+    "url": "/song-link/index.html"
+  },
+  {
+    "revision": "116d16c59e875429bf02",
+    "url": "/song-link/static/css/main.cc955ed1.chunk.css"
+  },
+  {
+    "revision": "b234fd20ce98cd5dea92",
+    "url": "/song-link/static/js/2.0334fb3b.chunk.js"
+  },
+  {
+    "revision": "116d16c59e875429bf02",
+    "url": "/song-link/static/js/main.50a01943.chunk.js"
+  },
+  {
+    "revision": "c57bacb572bd98bc7add",
+    "url": "/song-link/static/js/runtime-main.6e6c8c8a.js"
+  }
+]);
